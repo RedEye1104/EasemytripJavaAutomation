3e0ac69858b6e8ac77bbeb5e6d65b154fde98ba1
RedEye1104 <VishuChauhan11042000@gmail.com>
2026-03-21 21:28:20 +0530

    we have rearrage all the Xpaths to work it batter way

--- src/main/java/Pages_xpath/Listing_Page_Xpath.java
+++ src/main/java/Pages_xpath/Listing_Page_Xpath.java
@@ -3,15 +3,20 @@ import org.openqa.selenium.By;
 
 public class Listing_Page_Xpath {
     // closing the Upcoming Ads.
-    public static By close_ads = By.xpath("//*[@class=\"trmn ng-tns-c1899363251-1\"]");
+    public static By close_ads = By.xpath("//*[@class=\"trmn ng-tns-c2706753517-1\"]");
 
     //Enter the hotel name and select the first one hotel.(Spree Hotel Jaipur)
     public static By Search_hotel = By.xpath("//*[@id=\"filterPanel\"]/filter-panel/div/div[1]/div[1]/div[1]/input");
     public static By Select_hotel = By.xpath("//*[@id=\"hotelAutosuggest\"]/li[1]");
 
+    // adding closing ad again it is showing again.
+    public static By Close_ad_again = By.xpath("/html/body/app-root/div/hotel-srp/div[4]/div[1]");
 
-    // Select the popularity of the Hotels. (Low to High)
-    public static By click_popularity = By.xpath("//*[@class=\"drp-bx uparr ng-tns-c1899363251-1\"]");
+    // Select the popularity of the Hotels.
+    public static By click_popularity = By.xpath("/html/body/app-root/div/hotel-srp/div[3]/div[2]/div[1]/div[2]/div[1]/div");
+
+    // Select The Low to High popularity.
+    public static By Select_low_to_high = By.xpath("//*[@id=\"lth\"]");
 
     // Select Lowest price from filter
     public static By Click_Last_Minutes_Deals = By.xpath("//*[@id=\"filterPanel\"]/filter-panel/div/div[1]/div[2]/div/div/label[2]/div[2]/span");
