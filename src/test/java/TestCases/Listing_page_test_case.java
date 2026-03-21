6efe22e0be0545d167262b79f77cd3463aa69e1c
RedEye1104 <VishuChauhan11042000@gmail.com>
2026-03-21 20:31:59 +0530

    Popularity issue is fix now. Lisitng page is working as exected

--- src/test/java/TestCases/Listing_page_test_case.java
+++ src/test/java/TestCases/Listing_page_test_case.java
@@ -8,27 +8,37 @@ public class Listing_page_test_case extends setupdriver{
 
     @Test(dependsOnMethods = "Test_Home_page")
     public void Listing_page() throws InterruptedException{
-        driver.get(ListingURL);
+        driver.get(ListingURL);           //Spree Hotel Jaipur
 
         driver.findElement(Listing_Page_Xpath.close_ads).click();
 
-        driver.findElement(Listing_Page_Xpath.Search_hotel).sendKeys("Spree Hotel Jaipur");
-
-        Thread.sleep(2000);
-        driver.findElement(Listing_Page_Xpath.Select_hotel).click();
+        Thread.sleep(3000);
 
         driver.findElement(Listing_Page_Xpath.click_popularity).click();
+
+        Thread.sleep(3000);
+
+        driver.findElement(Listing_Page_Xpath.Select_low_to_high).click();
 
         driver.findElement(Listing_Page_Xpath.Click_Last_Minutes_Deals).click();
 
         driver.findElement(Listing_Page_Xpath.Reset_all_Filter).click();
 
-        driver.findElement(Listing_Page_Xpath.Hotel_name).getText();
+        driver.findElement(Listing_Page_Xpath.Search_hotel).sendKeys("Spree Hotel Jaipur");
 
-        driver.findElement(Listing_Page_Xpath.Hotel_price).getText();
+        driver.findElement(Listing_Page_Xpath.Search_hotel).click();
+
+       String Hotel_Name =  driver.findElement(Listing_Page_Xpath.Hotel_name).getText();
+       System.out.println("Selected Hotel Name is :-  "+Hotel_Name);
+
+       String Hotel_price =  driver.findElement(Listing_Page_Xpath.Hotel_price).getText();
+       System.out.println("Selected Hotel Price is :-  "+Hotel_price);
+
 
         driver.findElement(Listing_Page_Xpath.View_Rooms).click();
-        Thread.sleep(5000);
+
+        Thread.sleep(3000);
+
 
         DetailPage_URL = driver.getCurrentUrl();
         System.out.println(" This is The DetailPage URL:-    " + DetailPage_URL);
