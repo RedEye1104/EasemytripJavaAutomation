e996fff504ffd2ddf64f2a9343bb3be989fd3acc
RedEye1104 <VishuChauhan11042000@gmail.com>
2026-04-01 17:37:21 +0530

    Changes in price per night, change in price per night type section, change in book now add new xpath

--- src/main/java/Pages_xpath/Detail_Page_Xpath.java
+++ src/main/java/Pages_xpath/Detail_Page_Xpath.java
@@ -4,26 +4,29 @@ import org.openqa.selenium.By;
 
 
 public class Detail_Page_Xpath {
+    //Closing the Ad which is coming.
+    public static By Hotel_Ad = By.xpath("/html/body/app-root/div/hotel-details/div[3]/div[1]");
 
     // print Hotel information in summary.
-    public static By hotel_summary = By.xpath("//*[@class=\"txt-cap ng-tns-c3393003408-1\"]");
+    public static By hotel_summary = By.xpath("//*[@class=\"dprtinfo ng-tns-c1377921438-2 ng-star-inserted\"]");
+
     //Print hotel name.
-    public static By Hotel_Name = By.xpath("//*[@class=\"headcol mgr15 ng-tns-c1919069590-2\"]");
+    public static By Hotel_Name = By.xpath("//*[@class=\"headcol mgr15 ng-tns-c1377921438-2\"]");
 
     //Print Base price per night
-    public static By Price_per_night = By.xpath("/html/body/app-root/div/hotel-details/div[1]/div/image-slider/div/div[2]/div[2]/div[1]/div[2]/span[1]");
+    public static By Price_per_night = By.xpath("/html/body/app-root/div/hotel-details/div[1]/div/image-slider/div/div[2]/div[2]/div[1]/div[2]/div[3]/span");
 
     // Click on Select Room option
-    public static By Select_Room = By.xpath("//*[@class=\"ouline-btn ng-tns-c1919069590-2\"]");
+    public static By Select_Room = By.xpath("/html/body/app-root/div/hotel-details/div[1]/div/image-slider/div/div[2]/div[2]/div[3]/a[1]");
 
     //print Room Type
     public static By Room_type = By.xpath("//*[@id=\"2\"]/div[2]/div[1]/div/h4");
 
     //Print Room Benefit
-    public static By Benefit = By.xpath("//*[@id=\"2\"]/div[2]/div[2]/div[1]/div[1]/div");
+    public static By Benefit = By.xpath("//*[@id=\"2\"]/div[2]/div[2]/div[1]/div[1]/div/div");
 
     //Print price per night from room detail section
-    public static By Price_per_Night_typeSection = By.xpath("//*[@id=\"prcUserDiv0_0\"]/div[3]/div[2]");
+    public static By Price_per_Night_typeSection = By.xpath("//*[@id=\"prcUserDiv0_0\"]/div[3]/div[2]/span[1]");
 
     //Click on Overview
     public static By Click_overview = By.xpath("//*[@id=\"mainNav\"]/li[2]/a");
@@ -35,7 +38,7 @@ public class Detail_Page_Xpath {
     public static By Back_In_Rooms =  By.xpath("//*[@id=\"mainNav\"]/li[1]/a");
 
     //Click on Book Now.
-    public  static By Book_Now =  By.xpath("/html/body/app-root/div/hotel-details/div[3]/desktop-hotel-room-list/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/a");
+    public  static By Book_Now =  By.xpath("/html/body/app-root/div/hotel-details/div[2]/desktop-hotel-room-list/div/div/div[2]/div[2]/div[1]/div[2]/div[3]/div[1]/a");
 
 
 
