656d6649bc966ab0fc8c0da2a13dc58991c37d20
RedEye1104 <VishuChauhan11042000@gmail.com>
2026-04-07 20:23:57 +0530

    I have done with the Format of Travel page Testcase

--- src/test/java/TestCases/Traveler_page_Test_cases.java
+++ src/test/java/TestCases/Traveler_page_Test_cases.java
@@ -1,13 +1,13 @@
 package TestCases;
-
+import base_station.setupdriver;
 import org.testng.annotations.Test;
 
-public class Traveler_page_Test_cases {
+public class Traveler_page_Test_cases extends setupdriver {
 
-    @Test
-    public void Traveler_page_Test_cases()
+    @Test(dependsOnMethods = "Listing_page")
+    public void Traveler_page_Test_cases() throws InterruptedException
     {
-        System.out.println("Testing Traveler_page_Test_cases");
+
 
     }
 
